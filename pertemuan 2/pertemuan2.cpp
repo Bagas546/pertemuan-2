6742c8666c1afe39d7b0549b151994e1c7816ffd
Bagas546 <subiantarabagas@gmail.com>
2024-09-25 13:54:18 +0700

    Add files via upload

---
+++ pertemuan 2/pertemuan2.cpp
@@ -0,0 +1,29 @@
+ #include <iostream>
+
+using namespace std;
+
+int main(){
+    string nama, nim;
+    int usia;
+
+    cout << "Masukan nama: ";
+    cin >> nama;
+    cout << "Masukan nim: ";
+    cin >> nim;
+    cout << "masukan usia: ";
+    cin >> usia;
+
+    //luping atau perulangan
+    //for (int i=0; i < usia;i++)
+    //cout << nim << nama << endl;
+
+    //int i=0;
+    //do {
+    //cout << nama << endl;
+    //i++;
+    //}while (i < usia);
+
+
+
+    return 0;
+}
